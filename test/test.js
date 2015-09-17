2015692ac2512583838d3c3df219754650a877aa
Hafeez <feeziman007@hotmail.com>
2015-09-18 00:11:32 +1000

    renaming test file

--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var test = require('tape')
-var summer = require('../')
+var summer = require('../index')
 test('summer', function (t) {
     var winter = summer(-40)
     t.equal(winter(40), 0, 'should be equal')
